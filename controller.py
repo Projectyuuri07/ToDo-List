1968e55632653ad2fa82c19f1f6016bb8fc1c426
Projectyuuri07 <projectyuuri07@gmail.com>
2023-10-10 14:17:16 -0300

    Update

---
+++ controller.py
@@ -0,0 +1,49 @@
+from model import*
+from view import*
+
+class ControllerAdicionarTarefa():
+    def __init__(self, tarefa):
+        self.tarefa = tarefa
+        try:
+            todo.addTarefa(self.tarefa)
+            print("Tarefa adicionada com sucesso")
+        except Exception as e:
+            print(f"Erro ao adicionar tarefa: {e}")
+        print("")  
+
+class ControllerExcluirTarefa():
+    def __init__(self, excluir):
+        try:
+            excluir = int(excluir) - 1
+            if excluir < 0:
+                raise  ("Digite um número inteiro positivo para excluir a tarefa")
+            if excluir >= len(todo.lista):
+                raise ("Número de tarefa a excluir fora do alcance")
+            todo.removeTarefa(excluir)
+            print("Tarefa excluída com sucesso")
+        except Exception as erro:
+            print(f"Erro ao excluir tarefa: {erro}")
+
+
+class ControllerListarTarefa():
+    def __init__(self):
+        try:
+            ControllerLista = todo.listarTarefa()
+            if not ControllerLista:
+                print("Nenhuma tarefa encontrada.")
+            else:
+                cont = 1
+                for tarefa in ControllerLista:
+                    print(f"{cont} -- {tarefa}")
+                    cont += 1
+        except Exception as erro:
+            print(f"Erro: {erro}")
+
+todo = ToDO()
+
+def obter_opcao():
+        opcao = input("Digite a opção desejada: ")
+        if opcao.isdigit():
+            return int(opcao)
+        else:
+            print("Por favor, digite um número inteiro válido.")
---
+++ view.py
@@ -0,0 +1,44 @@
+from controller import*
+
+sair=True
+
+while sair == True:
+    print("Software de gerenciamento de tarefas")
+    print("1 -> Adicionar tarefa")
+    print("2 -> Excluir tarefa")
+    print("3 -> Listar tarefas")
+    print("4 -> Sair")
+    print("")
+    opcao = obter_opcao()
+
+    match opcao:
+        case 1:
+            limpar()
+            tarefa = input("Digite a tarefa: ")
+            adicionartarefa=ControllerAdicionarTarefa(tarefa)
+            parar()
+            limpar()
+
+        case 2:
+            limpar()
+            listarTarefa=ControllerListarTarefa()
+            excluir = (input("Digite o número da tarefa que deseja excluir: "))
+            excluirTarefa=ControllerExcluirTarefa(excluir)
+            listarTarefa=ControllerListarTarefa()
+            parar()
+            limpar()
+        case 3:
+            limpar()
+            listarTarefa=ControllerListarTarefa()
+            parar()
+            limpar()
+
+            
+        case 4:
+            sair=False
+        case _:
+            limpar()
+            print("Opção inválida")
+            print("")
+            parar()
+            limpar()
